0125ac9d00a9fc02c38236598722b267002d249d
rendnil <andrew.lindner.15@gmail.com>
2018-12-17 15:20:45 -0500

    added 8 more categories to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,14 @@ require "pry"
   food_api_id = 49
   mythology_api_id = 680
   science_api_id = 25
+  us_cities_api_id = 7
+  people_api_id = 442
+  television_api_id = 67
+  history_api_id = 114
+  pop_music_api_id = 770
+  universities_api_id = 672
+  literature_api_id = 574
+  movies_api_id = 309
 
 Category.all.destroy_all
 Question.all.destroy_all
@@ -24,6 +32,14 @@ puts "creating categories"
   food = Category.create(name: "Food")
   mythology = Category.create(name: "Mythology")
   science = Category.create(name: "Science")
+  us_cities = Category.create(name: "US Cities")
+  people = Category.create(name: "People")
+  television = Category.create(name: "Television")
+  history = Category.create(name: "History")
+  pop_music = Category.create(name: "Pop Music")
+  universities = Category.create(name: "Universities")
+  literature = Category.create(name: "Literature")
+  movies = Category.create(name: "Movies")
 puts "done creating categories"
 
 puts "making american history questions"
@@ -146,3 +162,223 @@ puts "making science questions"
     end
   end
 puts "done making science questions"
+
+puts "making us cities questions"
+  us_cities_api_url = "#{base_api_url}category=#{us_cities_api_id}&offset=0"
+  us_cities_question_data = RestClient.get(us_cities_api_url)
+  us_cities_question_data = JSON.parse(us_cities_question_data)
+
+  us_cities_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: us_cities.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+  us_cities_api_url = "#{base_api_url}category=#{us_cities_api_id}&offset=100"
+  us_cities_question_data = RestClient.get(us_cities_api_url)
+  us_cities_question_data = JSON.parse(us_cities_question_data)
+
+  us_cities_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: us_cities.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+  us_cities_api_url = "#{base_api_url}category=#{us_cities_api_id}&offset=200"
+  us_cities_question_data = RestClient.get(us_cities_api_url)
+  us_cities_question_data = JSON.parse(us_cities_question_data)
+
+  us_cities_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: us_cities.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+puts "done making us cities questions"
+
+puts "making people questions"
+  people_api_url = "#{base_api_url}category=#{people_api_id}&offset=0"
+  people_question_data = RestClient.get(people_api_url)
+  people_question_data = JSON.parse(people_question_data)
+
+  people_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: people.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+  people_api_url = "#{base_api_url}category=#{people_api_id}&offset=100"
+  people_question_data = RestClient.get(people_api_url)
+  people_question_data = JSON.parse(people_question_data)
+
+  people_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: people.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+  people_api_url = "#{base_api_url}category=#{people_api_id}&offset=200"
+  people_question_data = RestClient.get(people_api_url)
+  people_question_data = JSON.parse(people_question_data)
+
+  people_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: people.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+puts "done making people questions"
+
+puts "making television questions"
+  television_api_url = "#{base_api_url}category=#{television_api_id}&offset=0"
+  television_question_data = RestClient.get(television_api_url)
+  television_question_data = JSON.parse(television_question_data)
+
+  television_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: television.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+  television_api_url = "#{base_api_url}category=#{television_api_id}&offset=100"
+  television_question_data = RestClient.get(television_api_url)
+  television_question_data = JSON.parse(television_question_data)
+
+  television_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: television.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+  television_api_url = "#{base_api_url}category=#{television_api_id}&offset=200"
+  television_question_data = RestClient.get(television_api_url)
+  television_question_data = JSON.parse(television_question_data)
+
+  television_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: television.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+puts "done making television questions"
+
+puts "making history questions"
+  history_api_url = "#{base_api_url}category=#{history_api_id}&offset=0"
+  history_question_data = RestClient.get(history_api_url)
+  history_question_data = JSON.parse(history_question_data)
+
+  history_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: history.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+  history_api_url = "#{base_api_url}category=#{history_api_id}&offset=100"
+  history_question_data = RestClient.get(history_api_url)
+  history_question_data = JSON.parse(history_question_data)
+
+  history_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: history.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+  history_api_url = "#{base_api_url}category=#{history_api_id}&offset=200"
+  history_question_data = RestClient.get(history_api_url)
+  history_question_data = JSON.parse(history_question_data)
+
+  history_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: history.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+puts "done making history questions"
+
+puts "making pop music questions"
+  pop_music_api_url = "#{base_api_url}category=#{pop_music_api_id}&offset=0"
+  pop_music_question_data = RestClient.get(pop_music_api_url)
+  pop_music_question_data = JSON.parse(pop_music_question_data)
+
+  pop_music_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: pop_music.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+  pop_music_api_url = "#{base_api_url}category=#{pop_music_api_id}&offset=100"
+  pop_music_question_data = RestClient.get(pop_music_api_url)
+  pop_music_question_data = JSON.parse(pop_music_question_data)
+
+  pop_music_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: pop_music.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+puts "done making pop music questions"
+
+puts "making universities questions"
+  universities_api_url = "#{base_api_url}category=#{universities_api_id}&offset=0"
+  universities_question_data = RestClient.get(universities_api_url)
+  universities_question_data = JSON.parse(universities_question_data)
+
+  universities_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: universities.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+  universities_api_url = "#{base_api_url}category=#{universities_api_id}&offset=100"
+  universities_question_data = RestClient.get(universities_api_url)
+  universities_question_data = JSON.parse(universities_question_data)
+
+  universities_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: universities.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+puts "done making literature questions"
+
+puts "making literature questions"
+  literature_api_url = "#{base_api_url}category=#{literature_api_id}&offset=0"
+  literature_question_data = RestClient.get(literature_api_url)
+  literature_question_data = JSON.parse(literature_question_data)
+
+  literature_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: literature.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+  literature_api_url = "#{base_api_url}category=#{literature_api_id}&offset=100"
+  literature_question_data = RestClient.get(literature_api_url)
+  literature_question_data = JSON.parse(literature_question_data)
+
+  literature_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: literature.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+puts "done making literature questions"
+
+puts "making movies questions"
+  movies_api_url = "#{base_api_url}category=#{movies_api_id}&offset=0"
+  movies_question_data = RestClient.get(movies_api_url)
+  movies_question_data = JSON.parse(movies_question_data)
+
+  movies_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: movies.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+  movies_api_url = "#{base_api_url}category=#{movies_api_id}&offset=100"
+  movies_question_data = RestClient.get(movies_api_url)
+  movies_question_data = JSON.parse(movies_question_data)
+
+  movies_question_data.each do |question|
+    if (question["question"].length>1) && (question["answer"].length>1) && (question["answer"].include?("<") == false)
+      Question.create(category_id: movies.id, question: question["question"], answer: question["answer"], value: question["value"])
+    end
+  end
+
+puts "done making movies questions"
